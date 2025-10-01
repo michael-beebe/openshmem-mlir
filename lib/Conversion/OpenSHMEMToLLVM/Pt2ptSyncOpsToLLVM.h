4aafc897b29fb0b14fe290cd862b60c1884e03b0
michael-beebe <michaelbeebe@protonmail.com>
2025-09-30 22:33:03 -0500

    make format.sh less annoying

--- lib/Conversion/OpenSHMEMToLLVM/Pt2ptSyncOpsToLLVM.h
+++ lib/Conversion/OpenSHMEMToLLVM/Pt2ptSyncOpsToLLVM.h
@@ -1,5 +1,5 @@
 //===- Pt2ptSyncOpsToLLVM.h - Point-to-point synchronization ops conversion
-//patterns -*- C++ -*-===//
+// patterns -*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Find all .h and .cpp files in the project directory, excluding the llvm-project directory, and run clang-format on them
-find . -path ./llvm-project -prune -o \( -name "*.h" -o -name "*.cpp" \) -print | while read -r file; do
+# Find all .h and .cpp files in the project directory, excluding the
+# llvm-project directory, and run clang-format on them
+find . -path ./llvm-project -prune -o \( -name "*.h" -o -name "*.cpp" \) \
+  -print | while read -r file; do
   clang-format -i -style=LLVM "$file"
 done
